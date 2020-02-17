8398c9c6fbac27e02ab9d3bd95bc0f6b07e8ee32
Lucas <alonsosistemas@gmail.com>
2020-02-17 17:34:58 -0300

    0.5

--- node_js_celke/models/db.js
+++ node_js_celke/models/db.js
@@ -1,11 +1,11 @@
-const Sequelize = require('sequelize')
+const Sequelize = require("sequelize")
 
-const sequelize = new Sequelize('celke', 'root', 'epc@123', {
+const sequelize = new Sequelize('celke', 'root', 'epc@123',{
     host: 'localhost',
     dialect: 'mysql'
 })
 
-module.exports={
+module.exports = {
     Sequelize: Sequelize,
     sequelize: sequelize
 }
